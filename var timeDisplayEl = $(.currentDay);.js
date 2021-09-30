81396ce9acab2eccbf15809ed90a4cbd157d017a
andrewc411 <88997322+andrewc411@users.noreply.github.com>
2021-09-29 21:42:19 -0400

    Delete var timeDisplayEl = $(.currentDay);.js

--- var timeDisplayEl = $(.currentDay);.js
+++
@@ -1,8 +0,0 @@
-var timeDisplayEl = $('#currentDay');
-
-function displayTime(){
-    var rightNow = moment().format("[Today is] dddd");
-    timeDisplayEl.text(rightNow);
-}
-var dayWeek = today.format("[Today is] dddd")
-$("#2a").text(dayWeek);
